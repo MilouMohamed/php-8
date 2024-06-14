0afdf8fb7f2dcd02093934df0d0b0273caa67c4f
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-06-14 17:51:58 +0100

    Add files via upload

---
+++ MySQL/index_MySql.php
@@ -0,0 +1,28 @@
+<?php
+// MySQL  Elzero Web School   51 videos
+echo "MySQL <br>";  // 
+echo "V 1 et 2 ----------------------------<br>";
+// CMD    de preferance syntax nom des comande sql en MAJUSCULE
+// ------ create database if not exists Nombase;
+// ------ drop database if exists Nombase;
+
+
+
+echo "V 1 et 2 ----------------------------<br>";
+// Pour la creation d une Base de donnes  il y a trois Methodes
+
+// ---------------- M1 line cmd -------------
+// dans CMD (racourcie )  mysql -u root
+// add to path M:\Programmation\PHP 8\mysql\bin;
+// rechercher path dans windows => modifi path => add ..
+// show databases  ; ||  mysql -u(user) root -p(pass word) || create database toto;
+// use nomDataBase;  
+
+// ---------------- M2 PHP MyAdmin -------------
+//   url du navigateur http://localhost/phpmyadmin/
+// create base Nom Base et ulf8_general_ci
+
+// ---------------- M3  MySql Workbench  -------------
+//  Programme like phpMyAdmin
+
+?>
