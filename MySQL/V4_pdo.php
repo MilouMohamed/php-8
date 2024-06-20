ae9b5fa3e8e8ed7b32efa02e0639d069f9cffc3c
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-06-20 17:29:47 +0100

    Add files via upload

--- MySQL/V4_pdo.php
+++ MySQL/V4_pdo.php
@@ -23,6 +23,46 @@ echo " <br>You are Connected <br>";
 // $db->exec($q);
 
 
+// V7
+for ($i=0; $i < 1; $i++) { 
+  $prer=$db->prepare("insert into v_7 ( 	str_ng ) values ('N1')");
+  $prer->execute();
+  echo "   ".$i;
+}
+
+
+}catch(PDOException $msg) {
+ 
+    echo " <br>Probleme".$msg->getMessage()." <br>"; 
+}
+
+
+
+/*
+
+// v4  
+// define("CNX","mysql:host=localhost;dbname=products;");
+  define("CNX","mysql:host=localhost;dbname=V_5;");
+
+
+$dsn=CNX;
+$user="root";
+$pw="";
+$option=array(
+    PDO::MYSQL_ATTR_INIT_COMMAND =>"SET NAMES utf8",// Pour lettres Arabic
+);
+try {
+
+$db= new PDO($dsn,$user,$pw,$option);// start new connecsion with pdo class
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+echo " <br>You are Connected <br>";
+
+// $q="insert into items (nome) values ('Prod-44'),('Prod-55') ;";
+// $q="insert into items (nome) values ('منتج  88') ;";
+
+// $db->exec($q);
+
+
 // V5
 for ($i=0; $i < 500; $i++) { 
   $prer=$db->prepare("insert into numerc (nbr) values (1200)");
@@ -35,6 +75,10 @@ for ($i=0; $i < 500; $i++) {
  
     echo " <br>Probleme".$msg->getMessage()." <br>"; 
 }
+
+*/
+
+
 
 
 
--- MySQL/index_MySql.php
+++ MySQL/index_MySql.php
@@ -1,8 +1,76 @@
 <?php
 // MySQL  Elzero Web School   51 videos
 echo "MySQL <br>";  // 
+echo "V 12  ----------------------------<br>";
+//----------- Tables Advanced;
+// alter table t1 MODIFY tt98 int(11), DROP nm; //2 instruction 
+// change to utf8_general_ci all colunms
+//alter TABLE t1 CONVERT TO CHARACTER SET utf8;
+//alter TABLE t1 CONVERT TO CHARACTER SET LATIN1; 
+
+/*
+echo "V 11  ----------------------------<br>";
+//----------- Tables Rename Change Type;
+// -----------  ADD 
+// alter table t1 add nc blob after id; 
+// alter table t1 add ncBB blob FIRST;  
+
+// -----------  DROP 
+// alter table t1 drop ncbb; 
+
+// -----------  CHANGE   (name column and Type )
+// alter TABLE t1 CHANGE nm nm BINARY FIRST;// emplacement
+//alter table t1 change nm98 tt98 varchar(20) AFTER nm1;
+
+// -----------  MODIFY  ( Type )
+// alter TABLE t1 MODIFY nm3 char(20); // change type
+
+/*
+echo "V 10  ----------------------------<br>";
+//----------- Tables Rename Change Type;
+// Alter table t1 rename tab1
+// rename TABLE tone_ to t1 , tt to t22; 
+// alter table t11 engine =myisam ; change type of data
+
+
+/*
+echo "V 9  ----------------------------<br>";
+//----------- Tables Create Drop Show Status;
+echo " data Base V 9";
+// DESCRIBE TONE_;              voir Table
+// Show columns From Tone_;     voir Table
+// Show  fields From Tone_;     voir Table
+
+//show table status; All Info
+// show Create Table TOne_;  afficher le code de la creation
+
+/*
+echo "V 8  ----------------------------<br>";
+//----------- Deal With DataBases
+echo " data Base V 8" ;
+// Drop database if exists NomBD;
+// show databases like "NonBD"
+
+
+
+/*
+echo "V 7  ----------------------------<br>";
+//----------- Data Type : Striing
+// base v_5 table String 
+// Text            => Store String Like ma9al
+// BLOB            => Store  Binary Large Object (Image , file ....)
+
+
+// Char             0-255       fast(dynam RAM) exemle Mot fix 
+// VarChar          0-65 556    slow(Static RAM)        
+
+// ENUM    (Radio Button)          => Choix N1 Ou N2 Ou N3  
+// Set     (CheckBox )             => Choix N1 et N2 et  N3 (Multiple Choix)  
+require_once("V4_pdo.php");
+
+/*
 echo "V 6  ----------------------------<br>";
-//----------- Date & Time
+//----------- Data Type : Date & Time
 // V_5 table V_6 dat  
 // Date                 =>YYYYMMDD OU YYYY-MM-DD
 // DateTime             =>YYYY-MM-DD  HH:mm:ss
@@ -14,7 +82,7 @@ echo "V 6  ----------------------------<br>";
 
 /*
 echo "V 5  ----------------------------<br>";
-//----------- Data Type Numeric
+//----------- Data Type : Numeric
 // Tiny     Integer             Max Char(4)
 // Small    Integer             Max Char(6)
 // Medimum  Integer             Max Char(9)
