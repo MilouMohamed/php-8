a5362458d076673536ef5b2dd8e98df67fd73ab0
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-09-07 12:35:57 +0100

    Add files via upload

---
+++ PHP2024/laravel/index_l.php
@@ -0,0 +1,9 @@
+<?php
+
+
+echo "laravel";
+//composer create-project laravel/laravel formation_Mo_Ja ;
+
+// cd formation_mo_ja/
+// php artisan          ===> all cmd
+// php artisan  serve    ===> lance le serveur
