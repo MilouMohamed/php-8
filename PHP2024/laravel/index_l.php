87cafcbf71c7e05426b1a6a61c7034e0e0513c2e
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-09-12 17:48:51 +0100

    Add files via upload

--- PHP2024/laravel/index_l.php
+++ PHP2024/laravel/index_l.php
@@ -1,9 +1,112 @@
 <?php
-
-
 echo "laravel";
+// VIdeo      V25
+// Formulaire pour  Ajouter
+
+// VIdeo      V24
+// cadr Profile
+
+// VIdeo      V23
+// Show Profile details
+
+
+// VIdeo      V22
+// Pagination Router Condition(whire("id","\d+"))
+
+
+// VIdeo      V21
+// Profile Dans un Table Html
+// href={{route("homePage")}} et name("homePage")
+// $profilles=  profile::all();  dans View
+
+
+// Controller      V20
+//Documentation
+
+
+// Controller      V19  seeder
+// php artisan migrate:fresh --seed
+// php artisan make:factory ProfileFactory --model=profile (Pour Creer Une factory pour insere dans bd )
+
+  
+
+
+// Controller      V18
+// Creer des donnees de test Seending / Seender (insert)
+//php artisan db:seed (Pour inser dans databaseSeeder.php)
+//php artisan make:seeder ProfileSeeder
+//php artisan db:seed --class=ProfileSeeder (execute un  seul fichier )
+
+
+// Controller      V17
+// ORM  Model et Migrations (fichier .env (datebase et mysql))
+//php artisan make:model profile -m OU(--migration)
+//php artisan migrate (Pour cree les tables) pas de changemet si une table est cree
+
+
+
+// Controller      V16
+// ORM Object Relation Mapping => crud
+//php artisan make:model profile
+
+// Controller      V15
+// components Slots 
+
+
+// Controller      V14
+// components et Props 
+
+
+
+
+// Controller      V13
+// @extends ("layout.master") 
+// @section("title")
+// @yield("title")
+
+
+// Controller      V12
+// Layout  @extends("layout.master") html5
+
+// Controller      V11
+// blade   include  / once endOnce / onculeFirst ...
+
+// Controller      V10
+// blade (switch isset Empty)
+
+
+// Controller      V9
+// blade if foreach
+
+// Controller      v8
+// php artisan make:controller homeController
+// <h2>{{$home}}</h2> 
+//homeController::class end use namespace1 
+
+
+// Passage de donnees a la vue      v7
+// Request $request
+
+ 
+
 //composer create-project laravel/laravel formation_Mo_Ja ;
+
+
 
 // cd formation_mo_ja/
 // php artisan          ===> all cmd
 // php artisan  serve    ===> lance le serveur
+// Instal           v3
+// Routage          v6
+/****************************** 
+    Route web.php
+        GET     => lecture
+        POST    => Ajouter (Formulaire) 
+        PUT     => Modification Complet
+        PATCH   => Modification Partielle
+        DELLETE => Supprimer
+
+ Eexemple User => nom ="Nom1" , pren ="pre 1";
+               PATCH  selement nom
+               PUT    selement nom et pren
+        */
