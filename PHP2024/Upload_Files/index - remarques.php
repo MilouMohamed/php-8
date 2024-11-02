e8b0c8b3ee42eb97a79d893c2223b2336d60872e
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-11-02 13:11:52 +0100

    Add files via upload

--- PHP2024/Upload_Files/index - remarques.php
+++ PHP2024/Upload_Files/index - remarques.php
@@ -1,14 +1,32 @@
 
 
 <!-- Multiple Upload Script  -->
-<!-- ---- 7 VIDOES ----   -->
-<!-- v2 ---------------------------------- 
+<!-- ---- 7 VIDOES ----    
+---- v4 ---------------------------------- 
+Upload Multiples
+
+
+
+---- v3 ---------------------------------- 
+ errors 0 1 2 3 4 5 6
+
+
+---- v2 ---------------------------------- 
 $extentios = array("png", "jpg", "jpeg", "gif");
 
 $extent_img = explode('.', $name_img);
 $extent_img = strtolower(end($extent_img));
 divise les affectation probleme 
-
+------------------
+["error"] sont les suivantes :
+0 - Pas d'erreur, le fichier a été téléchargé avec succès.
+1 - Le fichier dépasse la taille maximale autorisée par php.ini (upload_max_filesize).
+2 - Le fichier dépasse la taille maximale autorisée par le formulaire HTML (MAX_FILE_SIZE).
+3 - Le fichier a été partiellement téléchargé.
+4 - Aucun fichier n'a été téléchargé.
+6 - Dossier temporaire manquant.
+7 - Échec de l'écriture du fichier sur le disque.
+8 - Une extension PHP a arrêté le téléchargement du fichier.
 
 -->
 <!-- v1 ---------------------------------- -->
--- PHP2024/Upload_Files/index.php
+++ PHP2024/Upload_Files/index.php
@@ -3,8 +3,150 @@
 ?>
 <br><br><br>
 <form method="post" enctype="multipart/form-data">
+    <label for="File">Files Multiple : </label>
+    <input type="file" width="200px" name="my-works[]" Multiple="multiple"> <br>
+    ----------------------------------<br>
+    <input type="submit" value="Envoyer">
+</form>
+<hr>
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    echo "OUi POST <br>";
+
+    $images_up = $_FILES["my-works"];
+    echo "<pre>";
+    //  print_r($_FILES["my-works"]);
+
+    $name_type_s = $images_up["type"];
+    $name_tmp_name_s = $images_up["tmp_name"];
+    $name_img_s = $images_up["name"];
+    $name_size_s = $images_up["size"];
+    $name_error_s = $images_up["error"];
+
+
+    // echo "Count " . count($name_img_s);
+    // echo "<br>";
+    $extentios = array("png", "jpg", "jpeg", "gif");
+
+
+    for ($i = 0; $i < count($name_img_s); $i++) {
+
+        $errors = array();
+
+        if ($name_error_s[$i] == 4) {
+            $errors[] = "<div> No File Uploded</div>";
+        } else {
+            if ($name_size_s[$i] > 1000_000)
+                $errors[] = " Grand Size !!!";
+            $typeImg=explode(".",$name_img_s[$i]);
+            $typeImg= strtolower(end($typeImg));
+            // echo "<br> typeImg $typeImg <br>";
+
+            if (!in_array($typeImg, $extentios))
+                $errors[] = " Pad de type Image !!!";
+
+        }
+
+
+ 
+if (empty($errors)) { 
+            move_uploaded_file($name_tmp_name_s[$i], __DIR__ . "\up\\" . $name_img_s[$i]);
+
+            echo "<div style='background-color:green;padding:5px; margin-bottom:10px'>Nom Fille $name_img_s[$i] \t\t\t\t Est Bien Uplode  " . ($i + 1) . " </div>";
+        } else {
+
+            for ($j = 0; $j < count($errors); $j++) { 
+                echo "<div style='background-color:orange;padding:7px 12px; 
+        margin-bottom:10px'>\tNom Fille " . $name_img_s[$i] . " \t\t Error  " . ($i + 1) . "  :$errors[$j] </div>";
+            } 
+
+        }
+
+    }
+
+}
+
+
+
+
+
+/*
+    echo "<pre>";
+    // print_r($image_up);
+    echo "</pre>";
+    echo "Name is   $name_img <br>";
+    echo "Type is   $name_type <br>";
+    echo "temp is   $name_tmp_name <br>";
+    echo "Size is   $name_size <br>";
+*/
+
+
+
+
+/*
+    $extentios = array("png", "jpg", "jpeg", "gif");
+
+    $extent_img = explode('.', $name_img);
+    $extent_img = strtolower(end($extent_img));
+
+
+
+    // print_r($extent_img);
+
+    $errors = array();
+
+    if ($name_error == 4) {
+        $errors[] = "<h2>Pas De Fichier Uplode !!!</h2>";
+    } else {
+
+        if ($name_size > 10_000):
+            $errors[] = "<h2> La taille Est Superieur de X </h2>";
+        endif;
+
+        if (!in_array($extent_img, $extentios)):
+            $errors[] = "<h2>Le fichier N Est Pas Image !!!</h2>";
+        endif;
+    }
+
+
+
+
+    if (empty($errors)) {
+        move_uploaded_file($name_tmp_name, __DIR__ . "\up\\" . $name_img);
+    } else {
+        foreach ($errors as $erro) {
+            echo $erro;
+        }
+    }
+
+
+
+}
+
+
+
+
+
+/*
+
+echo $_SERVER['DOCUMENT_ROOT'] . "<br>";
+echo __FILE__ . "<br>";
+;
+echo __DIR__ . "<br>";
+;
+
+single file
+<br>
+<?php
+?>
+<br><br><br>
+<form method="post" enctype="multipart/form-data">
+<label for="File">Files Multiple : </label>
+    <input type="file" width="200px" name="my-works[]" Multiple="multiple"> <br>
+    ----------------------------------<br>
     <label for="File">File : </label>
-    <input type="file" name="my-work"> <br>
+    <input type="file" width="200px" name="my-work" Multiple="multiple"> <br>
     ----------------------------------<br>
     <input type="submit" value="Envoyer">
 </form>
@@ -15,6 +157,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     echo "OUi POST <br>";
 
     $image_up = $_FILES["my-work"];
+    echo "<pre>";
+echo "Count ".count($_FILES);
+    print_r($_FILES["my-work"]);
 
     $name_type = $image_up["type"];
     $name_tmp_name = $image_up["tmp_name"];
@@ -82,4 +227,5 @@ echo __FILE__ . "<br>";
 echo __DIR__ . "<br>";
 ;
 */
+
 ?>
