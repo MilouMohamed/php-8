94b6d27a7af6562dba93867d86e52f4c1da440f2
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-11-11 17:57:25 +0100

    Add files via upload

--- PHP2024/SECURETE/indec_secur.php
+++ PHP2024/SECURETE/indec_secur.php
@@ -2,7 +2,102 @@
 
 echo "Is From Securete";
 echo "<br>";
+// Video 18
+// ini_set('display_errors', '0');// desacteve msg error
+// error_reporting(0);
 
+// v 10 https let s encrypt  free ssl
+
+
+// v 9 header location thene exit or die
+
+   header("location:test_php.php");
+// die(); or exit(); //Pour Arrete le script
+echo "<br>is File index v 18 videoS<br>";
+
+
+
+// v 8 htaccess (desactive Un Chemain)
+// ------ M1 dans fichie .htaccess
+# ceci est un commentaire   
+// Option -Indexes 
+// # Echo Erorr Message
+// ErrorDocument 403 "Hello You Cant Entre This Directory"
+
+// ------ M2 Ajoute des Index.php OU html dans les Folders
+
+
+
+
+/*
+// v 7 desable Error
+try {
+    // phpinfo();
+    require "test.php"  ;
+} catch (Exception $exp) {
+   echo "MY Error <br>";
+//    echo $exp."<br>";
+}
+
+
+
+
+
+
+/*
+// v 6 Hash password
+// password_hash et password_verify
+$pass="123400";
+
+$pass_hash =password_hash($pass,PASSWORD_DEFAULT);
+echo"HASH = ".$pass_hash;
+echo "<br><br><br>";
+if(password_verify("123400",$pass_hash)):
+echo "Oui meme";
+else:
+echo "Non Pas les  memes";
+endif;
+
+
+
+
+
+/*
+// anciane methode
+$md5="Osama00";
+$md5=md5($md5);
+echo "<br>---- ----- ------ ----- ---- ---- ----- ----- <br>";
+echo"MD5 = ".$md5;
+
+
+
+
+/*
+//  sql Injection
+// v 5 Prevent Remote File Injection 
+// include page dans votre page
+//serveur config redemarer .. php.ini allow_url_include=0
+
+if (isset($_GET["p"])) {
+    $page = $_GET["p"];
+    $pages = array("test_txt.txt", "test_php.php");
+    if (in_array($page, $pages)) {
+        echo "Oui dans Liste<br><br>";
+        include $page;
+    }else {
+        echo "Pas dans Liste  des Pages<br>";
+    }
+}
+
+ 
+// include "test_php.php";
+
+
+
+
+
+
+/*
 //  sql Injection
 // v 4   SQl Injection
 require_once "db.php";
